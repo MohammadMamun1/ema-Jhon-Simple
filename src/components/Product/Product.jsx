28f5afbeec058d91dfa004e9d2b2a7e9ca0d0d6d
Mohammad Mamun <md.mamun.bd147@gmail.com>
2024-04-20 04:06:58 +0600

    add to cart updated

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,6 +3,7 @@ import './Product.css';
 
 const Product = (props) => {
     const {img, name, seller, ratings, price} = props.product
+    const handelAddToCart = props.handelAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +13,9 @@ const Product = (props) => {
             <p>Manufacturar: {seller}</p>
             <p>Ratings: {ratings}</p>
            </div>
-           <button className='btn-cart'>Add to Cart</button>
+           <button onClick={()=>handelAddToCart(props.product)} className='btn-cart'>Add to Cart
+    
+           </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,16 @@ import './Shop.css';
 import Product from '../Product/Product';
 const Shop = () => {
     const [products , setProducts] = useState([]);
+    const [cart, setCart]=useState([]);
         useEffect(()=>{
             fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
         }, [])
+        const handelAddToCart = (product) => {
+            const newCart = [...cart, product];
+            setCart(newCart);
+        }
     
     return (
         <div className='shop-container'>
@@ -16,11 +21,13 @@ const Shop = () => {
                 products.map(product => <Product
                 key={product.id}
                 product = {product}
+                handelAddToCart = {handelAddToCart}
                 ></Product>)
             }
             </div>
             <div className='cart-container'>
                 <h4>Order Summary</h4>
+                <p>Total Cart : {cart.length}</p>
             </div>
 
         </div>
